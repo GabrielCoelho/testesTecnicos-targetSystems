67d43c8fd4d75682792879c14f7936ede157cc46
GabrielCoelho <gbcoe@outlook.com.br>
2025-01-10 17:56:06 -0300

    feat(rust -> java) Changed second question language

--- Pergunta01.md
+++ Pergunta01.md
@@ -9,3 +9,21 @@ Imprimir(SOMA);
 ## Ao final do processamento, qual será o valor da variável SOMA?
 
 Resposta: ==91==
+
+```c
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  int index = 13, k = 0, sum = 0;
+
+  while (k < index) {
+    k += 1;
+    sum = sum + k;
+  }
+
+  printf("%d", sum);
+  return EXIT_SUCCESS;
+}
+```
--- pergunta02.java
+++ pergunta02.java
@@ -1,10 +1,3 @@
-# Pergunta 02
-
-Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
-
-IMPORTANTE: Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
-
-```java
 public class pergunta02 {
 
     public static void main(String[] args) {
@@ -27,4 +20,3 @@ public class pergunta02 {
         return (x1Sqrt * x1Sqrt == x1) || (x2Sqrt * x2Sqrt == x2);
     }
 }
-```
